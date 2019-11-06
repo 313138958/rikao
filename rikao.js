4fdb6e7de8ccef381aab21bb3b50213399f91dd7
313138958 <313138958@qq.com>
2019-11-06 08:46:07 +0800

    小试牛刀

---
+++ rikao.js
@@ -0,0 +1,10 @@
+let Arr = ["a",'b','c',123,44,'g','5']
+let Brr = ["ss","dd","dw","ww"]
+let Fn = (arr) =>{
+    for(let a = 0 ; a<=arr.length-1; a++){
+        console.log(arr[a])
+    }
+}
+
+Fn(Arr)
+Fn(Brr)
